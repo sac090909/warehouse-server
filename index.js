c8821a1598059295a38d84775703bc2f74a584e7
Abu Hena Mohammad Hossain <abu_hena_mohammad_hossain@homedepot.com>
2022-05-04 17:23:23 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+var cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 4001;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dbfurniture1:NtP45fbQxsa5vBgh@cluster0.z12ej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollection = client.db("furnitureWarehouse").collection("items");
+
+    //GET - all
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+
+    //GET - one
+    app.get("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await itemsCollection.findOne(query);
+      res.send(result);
+    });
+
+    //POST - one
+    app.post("/inventory", async (req, res) => {
+      const newInventory = req.body;
+      console.log(newInventory);
+      const inventory = await itemsCollection.insertOne(newInventory);
+      res.send(inventory);
+    });
+
+    //PUT
+    app.put("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedQuantity = req.body;
+      const filter = { _id: ObjectId(id) };
+      console.log(updatedQuantity);
+      const updatedDoc = {
+        $set: {
+          quantity: updatedQuantity.quantity,
+          // about: updatedService.about,
+        },
+      };
+
+      const options = { upsert: true };
+      const result = await itemsCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    //DELETE
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await itemsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //GET - server test
+    app.get("/", (req, res) => {
+      res.send("furniture warehouse inventory");
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("db connected at port", port);
+});
