3d2dc93c611803ac53bcdbab878642a43f42f93c
Abu Hena Mohammad Hossain <abu_hena_mohammad_hossain@homedepot.com>
2022-05-04 21:13:02 -0500

    use env file

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://dbfurniture1:NtP45fbQxsa5vBgh@cluster0.z12ej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z12ej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
